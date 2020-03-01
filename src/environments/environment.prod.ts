3d43957f9c1f49743d0a1a82cf49b64e1d6230fc
David Mojicevic <david.mojicevic@eng.it>
2020-03-01 16:44:18 +0100

    Remove information

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,7 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyC7VPYqZ5eq6VjUJeQPg3rXc7rGcMjEvlo',
     authDomain: 'organicshop-62796.firebaseapp.com',
-    databaseURL: 'https://organicshop-62796.firebaseio.com',
     projectId: 'organicshop-62796',
     storageBucket: 'organicshop-62796.appspot.com',
     messagingSenderId: '1055629708147'
