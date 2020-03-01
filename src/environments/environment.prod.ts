70826adecfe6ebb10155ff5c23fb4dc983b973fd
BeringSea <30295250+BeringSea@users.noreply.github.com>
2020-03-01 17:08:05 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    authDomain: 'organicshop-62796.firebaseapp.com',
-    projectId: 'organicshop-62796',
-    storageBucket: 'organicshop-62796.appspot.com',
-    messagingSenderId: '1055629708147'
+    
   }
 };
